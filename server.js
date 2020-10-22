9c438e5eaf13fa3d3ea46321467387e9edfdc598
Nilsen <58581719+nreyes1202@users.noreply.github.com>
2020-10-21 20:37:53 -0600

    created new port 9000 as 8080 was already in use

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 9000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
@@ -15,7 +15,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgersController.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
